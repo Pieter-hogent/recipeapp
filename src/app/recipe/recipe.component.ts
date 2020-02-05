f9fdd84cfc25c0da58dd26f7c2af7acae7744fd8
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-02-05 11:50:54 +0100

    using a collection of data

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 	name: string;
+	ingredients: string[];
 
 	constructor() {
 		this.name = 'spaghetti';
+		this.ingredients = ['tomato', 'onion', 'celery', 'carrot', 'minced meat'];
 	}
 
 	ngOnInit() {}
