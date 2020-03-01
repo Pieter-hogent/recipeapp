0167d14c45e347518f1625af44be5a9a030ec943
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-03-01 16:49:49 +0100

    add ingredient as formarray (still with wrong validator)

--- src/app/recipe/add-recipe/add-recipe.component.ts
+++ src/app/recipe/add-recipe/add-recipe.component.ts
@@ -4,9 +4,11 @@ import {
   FormGroup,
   FormControl,
   Validators,
-  FormBuilder
+  FormBuilder,
+  FormArray
 } from '@angular/forms';
 import { Ingredient } from '../ingredient.model';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-recipe',
@@ -20,11 +22,36 @@ export class AddRecipeComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
+  get ingredients(): FormArray {
+    return <FormArray>this.recipe.get('ingredients');
+  }
   ngOnInit() {
     this.recipe = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       ingredients: this.fb.array([this.createIngredients()])
     });
+
+    this.ingredients.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe(ingList => {
+        // if the last entry's name is typed, add a new empty one
+        // if we're removing an entry's name, and there is an empty one after that one, remove the empty one
+        const lastElement = ingList[ingList.length - 1];
+
+        if (lastElement.name && lastElement.name.length > 2) {
+          this.ingredients.push(this.createIngredients());
+        } else if (ingList.length >= 2) {
+          const secondToLast = ingList[ingList.length - 2];
+          if (
+            !lastElement.name &&
+            !lastElement.amount &&
+            !lastElement.unit &&
+            (!secondToLast.name || secondToLast.name.length < 2)
+          ) {
+            this.ingredients.removeAt(this.ingredients.length - 1);
+          }
+        }
+      });
   }
 
   createIngredients(): FormGroup {
