af010eb5fd7b53a9a21b942664ca1dd62dd09d68
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-03-01 13:40:26 +0100

    add first validator

--- src/app/recipe/add-recipe/add-recipe.component.ts
+++ src/app/recipe/add-recipe/add-recipe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -15,7 +15,10 @@ export class AddRecipeComponent implements OnInit {
 
   ngOnInit() {
     this.recipe = new FormGroup({
-      name: new FormControl('risotto')
+      name: new FormControl('risotto', [
+        Validators.required,
+        Validators.minLength(2)
+      ])
     });
   }
 
