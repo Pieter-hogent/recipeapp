be18b2276748bbb123a1b8f25c8ca8eafa708e18
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-03-09 13:44:32 +0100

    add mock fixture and error case test

--- cypress/integration/examples/myfirsttest.spec.js
+++ cypress/integration/examples/myfirsttest.spec.js
@@ -1,7 +1,31 @@
 describe('My First Test', function() {
   it('our app runs', function() {
-    cy.visit('http://localhost:4200');
+    cy.visit('/');
     cy.get('[data-cy=filterInput]').type('sp');
     cy.get('[data-cy=recipeCard]').should('have.length', 1);
+  });
+
+  it('mock recipe get', function() {
+    cy.server({ delay: 1000 });
+    cy.route({
+      method: 'GET',
+      url: '/api/recipes',
+      status: 200,
+      response: 'fixture:recipes.json'
+    });
+
+    cy.visit('/');
+    cy.get('[data-cy=recipeCard]').should('have.length', 3);
+  });
+  it('on error should show error message', function() {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '/api/recipes',
+      status: 500,
+      response: 'ERROR'
+    });
+    cy.visit('/');
+    cy.get('[data-cy=appError]').should('be.visible');
   });
 });
