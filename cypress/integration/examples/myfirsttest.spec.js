f3574f360abf03c0fa093173f2b193d0a36a8bcf
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-03-09 13:39:24 +0100

    test the recipe list filter

--- cypress/integration/examples/myfirsttest.spec.js
+++ cypress/integration/examples/myfirsttest.spec.js
@@ -1,6 +1,7 @@
 describe('My First Test', function() {
   it('our app runs', function() {
     cy.visit('http://localhost:4200');
-    cy.get('button').should('be.disabled');
+    cy.get('[data-cy=filterInput]').type('sp');
+    cy.get('[data-cy=recipeCard]').should('have.length', 1);
   });
 });
