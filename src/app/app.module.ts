4d26a164c051390c69d0e17ceaedb1bbda20fb52
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2020-03-09 16:37:07 +0100

    add first routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { AddRecipeComponent } from './recipe/add-recipe/add-recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RecipeModule } from './recipe/recipe.module';
 import { MaterialModule } from './material/material.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'recipe/list', component: RecipeListComponent },
+  { path: 'recipe/add', component: AddRecipeComponent },
+  { path: '', redirectTo: 'recipe/list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, RecipeModule, MaterialModule],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [AppComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    RecipeModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
